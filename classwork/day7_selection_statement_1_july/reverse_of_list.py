853422ffc65852209bc6996d8e82359538f521b2
aiSumit-M <sumitmandalnew@gmail.com>
2026-07-01 10:59:20 +0530

    reverse_of_list.py

---
+++ classwork/day7_selection_statement_1_july/reverse_of_list.py
@@ -0,0 +1,31 @@
+'''------- Problem Statement: Reverse a List
+
+Write a Python program to create a list of numbers.
+Reverse the list using the reverse() method.
+Display the reversed list.
+-------------------------------------------------------------'''
+
+#------- Coding ---------------
+
+#create a list
+numbers = [30,90,70,50]
+
+print("Numbers are :")
+print(numbers)
+
+print("--------------------------------------------")
+
+#reverse the list
+numbers.reverse()
+
+print("After reversing the list :")
+print(numbers)
+
+#----------------------------------------------------
+'''Output :
+Numbers are :
+[30, 90, 70, 50]
+--------------------------------------------
+After reversing the list :
+[50, 70, 90, 30]
+--------------------------------------------'''
