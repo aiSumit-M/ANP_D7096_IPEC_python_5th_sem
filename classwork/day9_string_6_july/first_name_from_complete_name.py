bb4adea468b30e32317fcb97606a84b706b621a4
aiSumit-M <sumitmandalnew@gmail.com>
2026-07-06 11:41:17 +0530

    first_name_from_complete_name.py

---
+++ classwork/day9_string_6_july/first_name_from_complete_name.py
@@ -0,0 +1,32 @@
+'''------- Problem Statement: Display First Name
+
+Write a Python program to accept the complete name
+of a person from the user and display only the first
+name without using any library method.
+-------------------------------------------------------------'''
+
+#------- Coding ---------------
+
+#input complete name from the user
+name = input("Enter your complete name :")
+
+firstname = ""
+
+#to extract first name
+for ch in name:
+
+    if(ch == ' '):
+        break
+
+    firstname = firstname + ch
+
+print("--------------------------------------------")
+
+print("First Name :", firstname)
+
+#----------------------------------------------------
+'''Output :
+Enter your complete name : Sumit Kumar Mandal
+--------------------------------------------
+First Name : Sumit
+--------------------------------------------'''
